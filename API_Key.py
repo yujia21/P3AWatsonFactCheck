788355a8118e5aca7afe392a29bf8d399a48ace4
JeanBaptiste-Lamare <jblamare@gmail.com>
2017-01-26 10:59:49 +0100

    Completed Search_from_keywords

--- API_Key.py
+++ API_Key.py
@@ -1,5 +1,5 @@
 api_key_jb = 'd16af6322d6b6049602f298bb9510751a80e2ea7'
 api_key_yj = '6b686d275c4cce6fe46efcbffd38a73f526c921c'
-api_key_chosen = api_key_yj
+api_key_chosen = api_key_jb
 #api_key_alvo = ''
 
--- Articles_search.py
+++ Articles_search.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[10]:
+# In[31]:
 
 get_ipython().system('jupyter nbconvert --to script Articles_search.ipynb')
 
@@ -9,41 +9,59 @@ from watson_developer_cloud import AlchemyDataNewsV1
 import json
 
 
-# In[8]:
+# In[32]:
 
 from API_Key import *
+from Keyword_input import extract_keywords
 alchemy_data_news = AlchemyDataNewsV1(api_key = api_key_chosen)
 
 
-# In[8]:
+# In[33]:
 
 def search_from_keywords(keywords):
     
+    # keywords is a dictionnary with 'subjects' (dictionnary with 'Person', 'Country', 'Organization', 'Unsorted Subjects'),
+    # 'relations', 'objects', 'negations'
+    
+    interesting_keywords = []
+    for intermediate_list in list(keywords['subjects'].values()):
+        interesting_keywords += intermediate_list
+    
+    for word in keywords['objects']:
+        if word not in interesting_keywords:
+            interesting_keywords.append(word)
+    
     tts='A['
-    for keyword in keywords:
+    for keyword in interesting_keywords:
         tts += keyword+'^'
     tts = tts[:-1]+']'
-    
-    print(tts)
 
     results = alchemy_data_news.get_news_documents(
                                                    start='now-1d',
                                                    end='now',
                                                    return_fields=['enriched.url.title',
                                                                   'enriched.url.url',
+                                                                  'enriched.url.text',
                                                                   'enriched.url.author',
                                                                   'enriched.url.publicationDate'],
                                                    query_fields={'q.enriched.url.text': tts})
-    print(json.dumps(results, indent=2))
+    
+    #print(json.dumps(results, indent=2))
+    
+    text_list = [doc['source']['enriched']['url']['text'] for doc in results['result']['docs']]
+    
+    keywords_list = [extract_keywords(text) for text in text_list]
+    
+    return keywords_list
 
 
-# In[9]:
+# In[35]:
 
-''' # Comment if using from Interface, decomment to test.
-k = ['Trump','president']
+
+# Comment if using from Interface, decomment to test.
+k = {'subjects': {'Person': ['Trump'], 'Unsorted Subjects': []}, 'objects': ['president']}
 
 search_from_keywords(k)
-'''
 
 
 # In[ ]:
