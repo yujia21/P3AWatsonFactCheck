cd5d0b95f59c29b05b67c481837ab93f8ee262a7
yujia21 <odonut@gmail.com>
2017-03-02 15:57:39 +0100

    Updates to input

--- Keyword_input.py
+++ Keyword_input.py
@@ -3,7 +3,7 @@
 
 # In[2]:
 
-get_ipython().system(u'jupyter nbconvert --to script Keyword_input.ipynb')
+get_ipython().system('jupyter nbconvert --to script Keyword_input.ipynb')
 
 from watson_developer_cloud import AlchemyLanguageV1
 import json
@@ -15,14 +15,7 @@ alchemy_language = AlchemyLanguageV1(api_key = api_key_chosen)
 # In[4]:
 
 def extract_keywords(inputfact):
-    subjects={} #dictionary of people/organization/country
-    subjects['Person']=[]
-    subjects['Country']=[]
-    subjects['Organization']=[]
-    subjects['Unsorted Subjects']=[]
-        
-    relations=[] #list of relations/verbs
-    objects=[]
+    output=[]
         
     import re
     n_list=[]
@@ -35,68 +28,28 @@ def extract_keywords(inputfact):
 
     
     #keywords
-    response = alchemy_language.combined(text=inputfact,extract='entities,typed-rels',max_items=100)
+    response = alchemy_language.combined(text=inputfact,extract='typed-rels',max_items=100)
     if response['status'] == 'OK' : 
         
         
-        for entity in response['entities']:
-            e_type=entity['type']
-            if e_type in subjects : #either person, country or organization
-                subjects[e_type].append(entity['text'])
-            elif (e_type=='JobTitle'):
-                relations.append('worksAs')
-                objects.append(entity['text'])
-
         for relation in response['typedRelations']:
             for argument in relation['arguments']:
+                if argument['part']=='first' : 
+                    this_subject = argument['text']
+
                 if argument['part']=='second' :
-                    for entity in argument['entities']:
-                        subjects['Unsorted Subjects'].append(entity['text'])
-                if argument['part']=='second' :
-                    for entity in argument['entities']:
-                        objects.append(entity['text'])
-            relations.append(relation['type'])
+                    this_object = argument['text']
+            output.append((this_subject,this_object, relation['type']))
         
-        print('The subjects are: ')
-        for key in subjects.keys() :
-            if not (len(subjects[key]) == 0) :
-                print(key+" : ")
-                print(subjects[key])
-        print("")
-        
-        print("The relations and verbs involved are: ")
-        print(relations)
-        print("")
-        
-        print("The objects of these relations are: ")
-        print(objects)
-        print("")
     else:
         print('Error in keyword extaction call: ', response['statusInfo'])
     
-    if (negation) : 
-        print('')
-        print('## Negations ##')
-        if (len(n_list)==1) : 
-            print("There is a negation : ", end='')
-            print(n_list)
-        else : 
-            print("There are negations : ", end='')
-            print(n_list)
-
-
     # Prepositions ? 
 
     # Semantic query expansion
     # dbpedia : Quepy
     # Scoping problem - Political issues. Sample queries
-    print('')
-    print('')
-    
-    output = {'subjects' : subjects, 
-              'relations' : relations, 
-              'objects' : objects, 
-              'negations' : n_list}
+    print(output)   
     return output
 
 
